3c8fd7f284e2febded1b32b6ba31c9142821c805
Dominikus <dominikus-noah@live.de>
2021-09-30 22:36:10 +0200

    Copied framework project and added it to sulution as assert-package.

---
+++ Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(new[] {1,2,3,4,5}, Is.SubsetOf(new[] {1,2,3,4,5,6}));
+        }
+    }
+}
